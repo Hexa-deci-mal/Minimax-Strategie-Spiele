b950b06a809acb8c347106056744adecd64544e1
Lukas Eckert <lukasaaron@hotmail.com>
2021-05-26 14:41:00 +0200

    Utils for printing to console

---
+++ utilities/printUtils.py
@@ -0,0 +1,7 @@
+'''
+Utilities that provide easy console output options. Intended use is for creating test outputs and for providing easy to use error messages.
+'''
+
+# Prints a message to console using red color.
+def printErrorMsg(msg):
+    print(f'\033[91m{msg}\033[0m')
