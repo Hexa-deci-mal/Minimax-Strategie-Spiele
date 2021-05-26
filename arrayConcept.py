5b0dae221e72a76190f00b455a936dde2315d05e
Lukas Eckert <lukasaaron@hotmail.com>
2021-05-26 12:30:01 +0200

    2D Array Concept Dummy

---
+++ arrayConcept.py
@@ -0,0 +1,6 @@
+'''
+This concept aims to explore possible implementations for a 2d array data structure. This 2d array is meant to provide the application with a unified interface to transfer relevant data between
+different sub-components of the software.
+
+All code within this concept should be considered work in progress. Extreme changes may occur at any time without prior notice.
+'''
