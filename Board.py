cc304fc1402ec76306c3eb9e83a7b696d174d359
MaKoerner93 <koernermanuel@hotmail.de>
2021-05-31 08:19:42 +0200

    Stand3105

--- Board.py
+++ Board.py
@@ -43,10 +43,40 @@ def create_board_bauernschach():
     return Global_Vars.board
 
 
-# Global_Vars.board = create_board_checkers()
+def create_board_test():
+    #1 = KI, 2 = Mensch
+    Global_Vars.board = np.zeros((6,6))
+    Global_Vars.board[0][1] = 1
+    Global_Vars.board[0][3] = 1
+    Global_Vars.board[0][5] = 1
+    Global_Vars.board[1][0] = 1
+    Global_Vars.board[1][2] = 1
+    Global_Vars.board[1][4] = 1
 
-# print(Game.current_Player_Pos_List(2))
+    Global_Vars.board[5][1] = 2 
+    Global_Vars.board[5][3] = 2
+    Global_Vars.board[5][5] = 2
+    Global_Vars.board[4][0] = 2
+    Global_Vars.board[4][2] = 2
+    Global_Vars.board[4][4] = 2
+
+    Global_Vars.board[2][3] = 2
+    return Global_Vars.board
+
+
+######### TEST AREA ##################################################################################################
+Global_Vars.game_name = 'bauernschach'
+Global_Vars.board = create_board_checkers()
+Global_Vars.possible_moves = Game.get_Possible_Moves(Global_Vars.player, Global_Vars.board, Global_Vars.game_name)
+Global_Vars.pos_list_player = Game.current_Player_Pos_List(2)
+
+print(Global_Vars.game_name)
+print(Global_Vars.player)
+#print(Global_Vars.pos_list_player)
 print(Global_Vars.board)
+print(Global_Vars.possible_moves)
+#print(Global_Vars.board)
+######################################################################################################################
 
 
 
